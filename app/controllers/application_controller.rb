c60b13d2bce11d442a548b569bd2c96dc93cabb5
Steven McVey <smcvey1210@gmail.com>
2023-01-05 19:57:19 -0700

    commit tree 0f2ccf58b60be6c8abacba2f20b41c9a6e6494d5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,26 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+
+    review.destroy
+
+    review.to_json
+  end 
+
+  post '/reviews' do
+    review = Review.create(score: params[:score], comment: params[:comment], game_id: params[:game_id], created_at: params[:created_at], updated_at: params[:updated_at], user_id: params[:user_id])
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+
+    review.to_json
+  end
 end
